7431116160db880545dcd73cfb7b41f2713bf335
mcbarlowe <mcbarlowe@gmail.com>
2019-05-04 01:43:29 -0400

    First FT specced out in comments, and now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    # setUp methods of test classes are executed before each test
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    # tearDown methods run after each test method
+    def tearDown(self):
+        self.browser.quit()
+
+    # all test method names start with the actual word test
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # gets homepage
+        self.browser.get('http://localhost:8000')
+
+        # assertIn is a method from unittest class that checks to see if
+        # the first item is in the second. assertNotIn checks the opposite
+        self.assertIn('To-Do', self.browser.title)
+        # fail just fails no matter what and produces the error message
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    # this runs all my test classes when the scrip is run
+    unittest.main(warnings='ignore')
+
