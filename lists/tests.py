a0f7581f93f041922e8c50f298ca9e70f37b2ad7
mcbarlowe <mcbarlowe@gmail.com>
2019-05-10 21:37:31 -0400

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -25,8 +25,32 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+#check that one new item has been saved to the database
+#objects.count() is a shorthand for objects.all().count()
+        self.assertEqual(Item.objects.count(), 1)
+#objects.first() is the same as doing objects.all()[0]
+        new_item = Item.objects.first()
+#check that the item's text is correct
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
@@ -8,4 +9,13 @@ def home_page(request):
 #and then builds an HttpResponse based on the content of the template
 #Django templates main strength consist of substituting Python variables into
 #HTML text which is why the code uses render instead of manually reading the
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text',''),})
+    #item = Item()
+    #item.text = request.POST.get('item_text', '')
+    #item.save()
+#if the site call is a POST method then the site does this
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
