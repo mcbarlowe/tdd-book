3d4f58f95d246ea6d7ad7a5e7cc942678619f6e5
mcbarlowe <mcbarlowe@gmail.com>
2019-05-28 00:36:57 -0400

    Simplified home page

--- lists/views.py
+++ lists/views.py
@@ -15,7 +15,9 @@ def home_page(request):
 #if the site call is a POST method then the site does this
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
