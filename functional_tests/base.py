04dc2da94637a1eb9affcbae08d1745082d07a32
mcbarlowe <mcbarlowe@gmail.com>
2019-06-06 00:31:32 -0400

    Moved functional tests into their own individual files

---
+++ functional_tests/base.py
@@ -0,0 +1,38 @@
+'''Tests for our list website'''
+import os
+import time
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+MAX_WAIT = 10
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+    # setUp methods of test classes are executed before each test
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.liver_server_url = f'http://{staging_server}'
+
+    # tearDown methods run after each test method
+    def tearDown(self):
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            #do the test as normal if it works then return
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            #if there's an exception wait a short amount of time and then
+            #try the test again while the difference between times is less
+            # than max wait
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
